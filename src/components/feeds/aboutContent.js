53286c8747433ee3e6bf32b940daa4e947e75173
ckinmind <245160421@qq.com>
2017-02-09 17:57:31 +0800

    change about page

--- src/components/feeds/aboutContent.js
+++ src/components/feeds/aboutContent.js
@@ -8,14 +8,14 @@ const AboutContent = ()=> (
             </p>
 
             <h1 className="heading">Contributor</h1>
-            <p className="about-content"><a href="https://github.com/gokulkrishh">Gokul</a></p>
+            <p className="about-content"><a href="https://github.com/ckinmind">ckinmind</a></p>
 
             <h1 className="heading">Source</h1>
-            <p className="about-content"><a href="https://github.com/gokulkrishh/React-hacker-news">Github</a></p>
+            <p className="about-content"><a href="https://github.com/ckinmind/hackernews-react">Github</a></p>
 
             <h1 className="heading">
-                <iframe src="https://ghbtns.com/github-btn.html?user=gokulkrishh&repo=React-hacker-news&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="160px" height="30px"></iframe>
-                <iframe src="https://ghbtns.com/github-btn.html?user=gokulkrishh&repo=React-hacker-news&type=fork&count=true&size=large" frameBorder="0" scrolling="0" width="158px" height="30px"></iframe>
+                <iframe src="https://ghbtns.com/github-btn.html?user=ckinmind&repo=hackernews-react&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="160px" height="30px"></iframe>
+                <iframe src="https://ghbtns.com/github-btn.html?user=ckinmind&repo=hackernews-react&type=fork&count=true&size=large" frameBorder="0" scrolling="0" width="158px" height="30px"></iframe>
             </h1>
         </div>
     </div>
